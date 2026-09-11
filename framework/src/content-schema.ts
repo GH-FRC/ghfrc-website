1cf9cfa73de2342630ef10defdae89a1b460e048
Tony Liu <252731350+LYT259@users.noreply.github.com>
2026-09-11 20:33:16 +0800

    Allow event introductions and footer tagline to be omitted

--- framework/src/content-schema.ts
+++ framework/src/content-schema.ts
@@ -124,7 +124,7 @@ export const siteSchema = z.object({
     metaDescription: z.string().min(1),
     eyebrow: z.string().min(1),
     title: z.string().min(1),
-    introduction: z.string().min(1),
+    introduction: z.string().min(1).optional(),
     upcomingTitle: z.string().min(1),
     pastTitle: z.string().min(1),
     upcomingLabel: z.string().min(1),
@@ -138,7 +138,7 @@ export const siteSchema = z.object({
   }),
   footer: z.object({
     title: z.string().min(1),
-    placeholder: z.string().min(1),
+    placeholder: z.string().min(1).optional(),
   }),
 });
 
@@ -268,7 +268,7 @@ export const eventSchema = z.discriminatedUnion('entryType', [
   z.object({
     entryType: z.literal('event'),
     title: z.string().min(1),
-    summary: z.string().min(1),
+    summary: z.string().min(1).optional(),
     description: z.string().min(1),
     startsAt: z.coerce.date(),
     endsAt: z.coerce.date(),
